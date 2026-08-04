fb66d594402ea45183e499ec7a7c2fb601e9a996
andrearomeo74-cloud <andrearomeo74@gmail.com>
2026-08-04 17:56:33 +0200

    Implement relation discovery tools in __init__.py
    Added relation discovery tools to the Congruity Framework Core.

--- congruity/relations/__init__.py
+++ congruity/relations/__init__.py
@@ -1,1 +1,19 @@
+"""Relation discovery tools for Congruity Framework Core."""
 
+from congruity.relations.discovery import (
+    RelationDiscoveryError,
+    canonical_relation_id,
+    discover_spearman_relations,
+    estimate_spearman_relation,
+    generate_candidate_pairs,
+    relation_estimates_to_frame,
+)
+
+__all__ = [
+    "RelationDiscoveryError",
+    "canonical_relation_id",
+    "discover_spearman_relations",
+    "estimate_spearman_relation",
+    "generate_candidate_pairs",
+    "relation_estimates_to_frame",
+]
